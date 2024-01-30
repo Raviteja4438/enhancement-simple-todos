badd14d83da95fbcac6e5c62a3c3d8770bc6b943
Raviteja4438 <ravitejaanumula1@gmail.com>
2024-01-30 10:23:58 +0000

    simple todo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,6 +1,5 @@
 import {Component} from 'react'
 import TodoItem from '../TodoItem'
-
 import './index.css'
 
 const initialTodosList = [
@@ -38,34 +37,82 @@ const initialTodosList = [
   },
 ]
 
-// Write your code here
 class SimpleTodos extends Component {
   state = {
     todosList: initialTodosList,
+    newTodo: '',
   }
 
   deleteTodo = id => {
-    const {todosList} = this.state
-    const updatedTodosList = todosList.filter(eachTodo => eachTodo.id !== id)
+    this.setState(prevState => ({
+      todosList: prevState.todosList.filter(todo => todo.id !== id),
+    }))
+  }
 
-    this.setState({
-      todosList: updatedTodosList,
-    })
+  saveTodo = (id, updatedTitle) => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(todo =>
+        todo.id === id ? {...todo, title: updatedTitle} : todo,
+      ),
+    }))
+  }
+
+  addTodo = () => {
+    const {newTodo} = this.state
+    const todoInput = newTodo.trim()
+    if (todoInput !== '') {
+      let todoTitle = todoInput
+      let count = 1
+      const lastSpaceIndex = todoInput.lastIndexOf(' ')
+      if (lastSpaceIndex !== -1) {
+        const lastWord = todoInput.substring(lastSpaceIndex + 1)
+        const num = parseInt(lastWord, 10)
+        if (!Number.isNaN(num)) {
+          todoTitle = todoInput.substring(0, lastSpaceIndex)
+          count = num
+        }
+      }
+      const newTodos = Array.from({length: count}, (_, index) => ({
+        id: Date.now() + index,
+        title: todoTitle,
+      }))
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, ...newTodos],
+        newTodo: '',
+      }))
+    }
+  }
+
+  handleChange = e => {
+    this.setState({newTodo: e.target.value})
   }
 
   render() {
-    const {todosList} = this.state
+    const {todosList, newTodo} = this.state
 
     return (
       <div className="app-container">
         <div className="simple-todos-container">
           <h1 className="heading">Simple Todos</h1>
+          <div>
+            <input
+              type="text"
+              id="input-box"
+              placeholder="Add your text"
+              value={newTodo}
+              onChange={this.handleChange}
+            />
+            <button type="button" onClick={this.addTodo}>
+              Add
+            </button>
+          </div>
           <ul className="todos-list">
-            {todosList.map(eachTodo => (
+            {todosList.map(todo => (
               <TodoItem
-                key={eachTodo.id}
-                todoDetails={eachTodo}
+                key={todo.id}
+                todo={todo}
                 deleteTodo={this.deleteTodo}
+                saveTodo={this.saveTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,20 +1,61 @@
-// Write your code here
+import {useState} from 'react'
 import './index.css'
 
-const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
+const TodoItem = ({todo, deleteTodo, saveTodo}) => {
+  const [editing, setEditing] = useState(false)
+  const [updatedTitle, setUpdatedTitle] = useState(todo.title)
 
   const onDeleteTodo = () => {
-    deleteTodo(id)
+    deleteTodo(todo.id)
+  }
+
+  const onToggleEdit = () => {
+    setEditing(!editing)
+  }
+
+  const onSaveEdit = () => {
+    saveTodo(todo.id, updatedTitle)
+    setEditing(false)
+  }
+
+  const onChangeTitle = e => {
+    setUpdatedTitle(e.target.value)
+  }
+
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      onSaveEdit()
+    }
   }
 
   return (
     <li className="todo-item">
-      <p className="title">{title}</p>
-      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
-        Delete
-      </button>
+      {editing ? (
+        <>
+          <input
+            type="text"
+            value={updatedTitle}
+            onChange={onChangeTitle}
+            onBlur={onSaveEdit}
+            onKeyPress={handleKeyPress}
+          />
+          <button type="button" className="save-btn" onClick={onSaveEdit}>
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <p className={todo.completed ? 'title completed' : 'title'}>
+            {todo.title}
+          </p>
+          <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+            Delete
+          </button>
+          <button type="button" className="edit-btn" onClick={onToggleEdit}>
+            Edit
+          </button>
+        </>
+      )}
     </li>
   )
 }
